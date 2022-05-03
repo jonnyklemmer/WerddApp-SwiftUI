5d761875a61ba6f04a4a74ffd7376b592cc9b2bc
jonnyklemmer <jonnyklemmer@gmail.com>
2022-05-02 22:06:35 -0700

    Initial Commit

---
+++ Werdd/WerddApp.swift
@@ -0,0 +1,17 @@
+//
+//  WerddApp.swift
+//  Werdd
+//
+//  Created by Jonny Klemmer on 5/2/22.
+//
+
+import SwiftUI
+
+@main
+struct WerddApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
